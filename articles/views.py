d598272d9c6d604879d714d6f1e7d0115737f861
ERIC.FY.CHENG <>
2024-12-12 10:30:06 +0800

    show articles from DB; adjust style

--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from articles.models import Article
 
 # Create your views here.
 def show(request):
-    results = [
-        {'date': 'test',
-         'content': 'Test content',
-         'create_at': 123,
-         'update_at': 123}
-    ]
-    return render(request, 'articles/show.html', locals())
+    articles = Article.objects.all()
+    results = []
+    for a in articles:
+        results.append(
+            {'date': a.date,
+            'content': a.content,
+            'create_at': a.create_at,
+            'update_at': a.update_at}
+        )
+    return render(request, 'articles/show.html', {'results': results})
