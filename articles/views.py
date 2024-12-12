b6fb19ba32c6c596adf2622c71adf7921686252c
ERIC.FY.CHENG <>
2024-12-12 10:38:30 +0800

    fix time format; adjust style

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils.timezone import make_aware
+from datetime import datetime
 from articles.models import Article
+
 
 # Create your views here.
 def show(request):
@@ -9,7 +12,7 @@ def show(request):
         results.append(
             {'date': a.date,
             'content': a.content,
-            'create_at': a.create_at,
-            'update_at': a.update_at}
+            'create_at': make_aware(datetime.fromtimestamp(a.create_at)),
+            'update_at': make_aware(datetime.fromtimestamp(a.update_at))}
         )
     return render(request, 'articles/show.html', {'results': results})
