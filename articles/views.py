849be7988e3c700f58beee9f9f4bb36eb077db34
ERIC.FY.CHENG <>
2024-12-12 09:43:38 +0800

    Add html and url for displaying articles

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show(request):
+    results = [
+        {'date': 'test',
+         'content': 'Test content',
+         'create_at': 123,
+         'update_at': 123}
+    ]
+    return render(request, 'home.html', locals())
