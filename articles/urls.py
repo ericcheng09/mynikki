a535ae791690ed17cbb98bed11a2b423714d6ea7
ERIC.FY.CHENG <>
2025-01-03 10:03:31 +0800

    FEAT: Using two different views. One show list of diaries and some content with link. The other one show all content of diary in a page.

--- articles/urls.py
+++ articles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show, name='article_show'),
+    path('', views.show_all, name='all_article'),
+    path('<int:id>', views.detail, name='article_detail'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -2,17 +2,46 @@ from django.shortcuts import render
 from django.utils.timezone import make_aware
 from datetime import datetime
 from articles.models import Article
+from django.http import HttpResponseNotAllowed, Http404
 
+SORT_BY_OPTIONS = ['id', 'create_at', 'update_at']
 
 # Create your views here.
-def show(request):
+def show_all(request):
+    if request.method != 'GET':
+        return HttpResponseNotAllowed()
+    
+    sort_by = request.GET.get('sort_by', 'id')
+    sort_by = sort_by.lower()
+    if sort_by not in SORT_BY_OPTIONS:
+        sort_by = 'id'
+     
     articles = Article.objects.all()
     results = []
+
     for a in articles:
         results.append(
-            {'date': a.date,
-            'content': a.content,
+            {
+            'id': a.id,
+            'date': a.date,
+            'content': a.content[:20] + '...',
             'create_at': make_aware(datetime.fromtimestamp(a.create_at)),
             'update_at': make_aware(datetime.fromtimestamp(a.update_at))}
         )
-    return render(request, 'articles/show.html', {'results': results})
+    
+    results = sorted(results, key= lambda r: r[sort_by], reverse=True)    
+    return render(request, 'articles/list.html', {'results': results})
+
+
+def detail(request, id):
+    article = Article.objects.get(pk=id)
+    if article:
+        r = {
+            'date': article.date,
+            'content': article.content,
+            'create_at': make_aware(datetime.fromtimestamp(article.create_at)),
+            'update_at': make_aware(datetime.fromtimestamp(article.update_at))
+            }
+        return render(request, 'articles/show.html', r)
+  
+    return Http404()
